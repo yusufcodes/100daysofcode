59bfad078d9f44409e3ebbba6f6ff3102dfef55f
yusufcodes <yusufcodes@outlook.com>
2018-08-31 17:34:15 +0100

    Wrote my solution to the expert challenge

--- Day 5/Section 5 - Coding Challenge/Expert Solution/My Solution/script.js
+++ Day 5/Section 5 - Coding Challenge/Expert Solution/My Solution/script.js
@@ -1,16 +1,7 @@
-
-
-//Write method into the prototype property of Question
-
-/* What I thought:
-Question.prototype = function askQuestion()
-{
-
-}
-How to actually do it (below)*/
-
 (function()
 {
+    var userScore = 0;
+
     function Question(question, answers, correctAnswer)
     {
         this.question = question;
@@ -27,29 +18,47 @@ How to actually do it (below)*/
         }
     }
     
-    Question.prototype.checkAnswer = function()
+    Question.prototype.checkAnswer = function(answer)
     {
-        if (answer === this.correctAnswer)
+
+        if(answer !== 'quit')
         {
-            console.log('Correct Answer');
-        }
+            if (answer === this.correctAnswer.toString())
+            {
+                console.log('Correct Answer');
+                userScore++;
+            }
     
+            else
+            {
+                console.log('Incorrect Answer');
+            }
+
+            init();
+        }
+
         else
         {
-            console.log('Incorrect Answer');
+            console.log('Game ended! Your score was: '+userScore);
         }
+        
     }
     
-    var q1 = new Question('Is JavaScript the coolest programming language?', ['Yes', 'No'], 0);
-    var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
-    var q3 = new Question('What best describes coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
-    
-    var questionsToAsk = [q1, q2, q3];
-    var num = Math.floor(Math.random() * questionsToAsk.length);
-    
-    questionsToAsk[num].displayQuestion();
-    
-    var answer = parseInt(prompt('Please select the correct answer'));
-    
-    questionsToAsk[num].checkAnswer();222
+    function init()
+    {
+        var q1 = new Question('Is JavaScript the coolest programming language?', ['Yes', 'No'], 0);
+        var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+        var q3 = new Question('What best describes coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+        var questionsToAsk = [q1, q2, q3];
+        var num = Math.floor(Math.random() * questionsToAsk.length);
+        
+        questionsToAsk[num].displayQuestion();
+        
+        var answer = (prompt('Please write the correct answer, type \'quit\' to exit.'));
+        
+        questionsToAsk[num].checkAnswer(answer);
+    }
+
+    init();
 })();
