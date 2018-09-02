68791f1204a9eb6e11832bd42aee34f622beb3df
yusufcodes <yusufcodes@outlook.com>
2018-09-02 06:50:15 +0100

    Added Day 7 files, updated Day 6 JS file (forgot to commit last night)

---
+++ Day 7/script.js
@@ -0,0 +1,33 @@
+/* Closures */
+
+//Example 1
+function numberGenerator()
+{
+    var num = 1; //Free variable, used in closure
+
+    function checkNumber()
+    {
+        console.log(num);
+    }
+
+    num++;
+    return checkNumber;
+}
+
+var number = numberGenerator();
+number();
+
+function sayHello()
+{
+    var say = function()
+    {
+        console.log(hello);
+    }
+
+    var hello = 'Hello, world';
+    return say;
+}
+
+var sayHelloClosure = sayHello();
+sayHelloClosure;
+
