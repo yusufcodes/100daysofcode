ea72ee5fb31fb87656c722b4199d610c3a8e41e6
yusufcodes <yusufcodes@outlook.com>
2018-09-05 09:14:20 +0100

    Added init function

--- Day 10/app.js
+++ Day 10/app.js
@@ -2,12 +2,12 @@
 // Data Controller
 var budgetController = (function()
 {
-    //Data controller code
+    // Data controller code
 
 })();
 
 
-//UI Controller
+// UI Controller
 var UIController = (function() 
 {
     var DOMstrings = 
@@ -22,10 +22,10 @@ var UIController = (function()
         getInput: function()
         {
             return {
-                type: document.querySelector(DOMstrings.inputType).value, //income or expense
-                description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value,
-            };
+            type: document.querySelector(DOMstrings.inputType).value, //income or expense
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+            }
         },
 
         getDOMstrings: function()
@@ -33,17 +33,26 @@ var UIController = (function()
             return DOMstrings;
         }
     };
-
-
 })();
-
-
 
 //Main controller - connects the UI and Data modules
 //How? They are passed in as parameters
 var controller = (function(budgetCtrl, UICtrl)
 {
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function()
+    {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event)
+        {
+            if (event.keyCode === 13 || event.which === 13) //which property - older browsers
+            {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function()
     {
@@ -54,18 +63,16 @@ var controller = (function(budgetCtrl, UICtrl)
         // 3.Add item to the UI
         // 4. Calculate the budget
         // 5. Display budget on UI
+    };
 
-    }
-
-    document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event)
-    {
-        if (event.keyCode === 13 || event.which === 13) //which property - older browsers
+    return {
+        init: function() 
         {
-            ctrlAddItem();
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    });
-    
+    };
 
 })(budgetController, UIController);
+
+controller.init();
