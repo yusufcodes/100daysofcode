0a3bbf59f8f6b2bb2109a87d5cddd9551f5ccd9c
yusufcodes <yusufcodes@outlook.com>
2018-09-05 09:31:18 +0100

    Added code to the budget controller

--- Day 10/app.js
+++ Day 10/app.js
@@ -2,7 +2,39 @@
 // Data Controller
 var budgetController = (function()
 {
-    // Data controller code
+    var Expense = function(id, description, value)
+    {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value)
+    {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var allExpenses = [];
+    var allIncomes = [];
+    var totalExpenses = 0;
+
+    var data = 
+    {
+        allItems:
+        {
+            exp: [],
+            inc: []
+        },
+
+        totals:
+        {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    
 
 })();
 
