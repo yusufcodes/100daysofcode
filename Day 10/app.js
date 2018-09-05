b694b8174c98cbeae5620eadb6c9b77e4fedb00b
yusufcodes <yusufcodes@outlook.com>
2018-09-05 07:53:00 +0100

    Added code for Hour 1 and wrote progress in Day_10

--- Day 10/app.js
+++ Day 10/app.js
@@ -1,40 +1,56 @@
+
 // Data Controller
 var budgetController = (function()
 {
-    var x = 23;
-
-    var add = function(a)
-    {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b)
-        {
-            return add(b);
-        }
-    }
+    //Data controller code
 
 })();
+
 
 //UI Controller
 var UIController = (function() 
 {
-    //Code to handle UI
+    return {
+        getInput: function()
+        {
+            return {
+            type: document.querySelector('.add__type').value, //income or expense
+            description: document.querySelector('.add__description').value,
+            value: document.querySelector('.add__value').value
+            };
+        }
+    };
+
 
 })();
+
+
 
 //Main controller - connects the UI and Data modules
 //How? They are passed in as parameters
 var controller = (function(budgetCtrl, UICtrl)
 {
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function()
+    {
+        // 1. Get the input data from the field
+        var input = UIController.getInput();
+        console.log(input);
+        // 2. Add item to the budget controller
+        // 3.Add item to the UI
+        // 4. Calculate the budget
+        // 5. Display budget on UI
 
-    return {
-        anotherPublic: function()
-        {
-            console.log(z);
-        }
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event)
+    {
+        if (event.keyCode === 13 || event.which === 13) //which property - older browsers
+        {
+            ctrlAddItem();
+        }
+    });
+    
 
 })(budgetController, UIController);
